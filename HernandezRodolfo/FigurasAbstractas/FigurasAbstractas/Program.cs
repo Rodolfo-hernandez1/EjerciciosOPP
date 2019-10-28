8c759fce1ef1ebea543597f8e37d7339943472b7
Rodolfo Hernandez <rodolfoh94@gmail.com>
2019-10-27 18:17:34 -0700

    FigurastTerminada

--- HernandezRodolfo/FigurasAbstractas/FigurasAbstractas/Program.cs
+++ HernandezRodolfo/FigurasAbstractas/FigurasAbstractas/Program.cs
@@ -3,52 +3,119 @@ using System.Collections.Generic;
 
 namespace figura
 {
-    class Figura 
+     abstract class Figura 
     {
         protected int x;
         protected int y;
         protected string color;
 
-        public Figura(int x, int y, string c){
-            this.x = x; this.y = y; color = c;
+        public Figura(int x, int y, string color){
+            this.x = x; this.y = y; this.color = color;
         }
 
-        public void dibuja()
-        {
-            Console.WriteLine("Se dibuja una figura color {0}", 
-            color);
+        public abstract void dibuja();
+        
+    
+
+       // public abstract  void printColor();
+         
+    
+    }
+
+    class Circulo:Figura 
+    {
+        public Circulo(int x, int y, string color):base(x , y, color){
         }
 
-        public void printColor() {
-            Console.WriteLine(color);
+        public override void dibuja(){
+            Console.WriteLine("Se dibuja un circulo en la ubicacion x= {0}, y ={1}, color {2}",x,y,color);
         }
     }
 
-    class Circulo : Figura {
-        public Circulo(int x, int y, string c):base(x , y, c){
+    class Rect:Figura 
+    {
+        public Rect(int x, int y, string color):base(x , y, color){
+            
         }
 
-        public new void dibuja(){
-            Console.WriteLine("Se dibuja un circulo {0}", color);
+        public override void dibuja(){
+            Console.WriteLine("Se dibuja un Cuadrado  en la ubicacion x= {0}, y = {1}, del color {2}",x,y,color);
         }
+
+    }
+    class Triangulo:Figura 
+    {
+        public Triangulo(int x, int y, string color):base(x , y, color){
+            
+        }
+
+        public override void dibuja(){
+            Console.WriteLine("Se dibuja un Triangulo  en la ubicacion x= {0}, y = {1}, del color {2}",x,y,color);
+        }
+
+    }
+    class Cuadrado:Figura 
+    {
+        public Cuadrado(int x, int y, string color):base(x , y, color){
+            
+        }
+
+        public override void dibuja(){
+            Console.WriteLine("Se dibuja un Cuadrado  en la ubicacion x= {0}, y = {1}, del color {2}",x,y,color);
+        }
+
+    }
+    class Hexagono:Figura 
+    {
+        public Hexagono(int x, int y, string color):base(x , y, color){
+            
+        }
+
+        public override void dibuja(){
+            Console.WriteLine("Se dibuja un Hexagono  en la ubicacion x= {0}, y = {1}, del color {2}",x,y,color);
+        }
+
+    }
+    class Trapecio:Figura 
+    {
+        public Trapecio(int x, int y, string color):base(x , y, color){
+            
+        }
+
+        public override void dibuja(){
+            Console.WriteLine("Se dibuja un Trapecio  en la ubicacion x= {0}, y = {1}, del color {2}",x,y,color);
+        }
+
     }
 
-    class Rect : Figura {
-        public Rect(int x, int y, string c):base(x , y, c){
-            }
+    class Octagono:Figura 
+    {
+        public Octagono(int x, int y, string color):base(x , y, color){
+            
         }
+
+        public override void dibuja(){
+            Console.WriteLine("Se dibuja un Octagono  en la ubicacion x= {0}, y = {1}, del color {2}",x,y,color);
+        }
+
+    }
     class Program{
-        static void Main(string[] args){
-            List<Figura> figuras = new List<Figura>();
-            figuras.Add(new Circulo(12,13,"verde")) ;
-            figuras.Add(new Rect(12,13,"azul")) ;
-            figuras.Add(new Rect(12,13,"rojo")) ;
-            figuras.Add(new Circulo(12,13,"rojo")) ;
-            foreach (var item in figuras){
-                item.dibuja();
-            }    
-            Circulo r = new Circulo(10,10,"rojo");   
-            r.dibuja();
+        static void Main(string[] args)
+        {
+           List<Figura>  fig = new List <Figura>();
+           fig.Add(new Circulo(10,20,"Rojo"));
+           fig.Add(new Rect(15,25,"Azul"));
+           fig.Add(new Triangulo(20,22,"Verde"));
+           fig.Add(new Cuadrado(11,22,"Morado"));
+           fig.Add(new Hexagono(20,30,"Amarillo"));
+           fig.Add(new Trapecio(29,35,"Negro"));
+           fig.Add(new Octagono(29,33,"Blanco"));
+
+           foreach(var figu in fig)
+           {
+               figu.dibuja();
+           }
+
             }
         }
 }
